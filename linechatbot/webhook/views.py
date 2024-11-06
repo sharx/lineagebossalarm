466b3aba87e8ca4f3d188aa84f48f3e634645c3b
s971511 <s971511@gmail.com>
2024-11-07 03:07:35 +0800

    add import for csrf_exempt

--- linechatbot/webhook/views.py
+++ linechatbot/webhook/views.py
@@ -5,6 +5,7 @@ import base64
 import hashlib
 import hmac
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 channel_secret = '2f97d3e3a877f6d13a8407793fa1caf5' # Channel secret string
 channel_id = '2006537537' # Channel ID string
